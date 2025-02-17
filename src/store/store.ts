7b4aff479e32fd566399e22e609f0864e1069313
omkar <opwebdev01@gmail.com>
2025-02-17 14:47:26 +0530

    latest change

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
-import { authStore, IAuthStore } from "./Auth";
+import { authStore, IAuthStore } from "./auth";
 import { hydarteStore, IHydrateStore } from "./hydrate";
 import { cartStore, ICartStore } from "./cart";
 

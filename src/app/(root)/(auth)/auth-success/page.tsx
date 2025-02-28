bd127b834bc9518027922ac04cc860d41a563f7c
Omkar Pawar <opwebdev01@gmail.com>
2025-02-28 09:58:55 +0530

    aa

--- src/app/(root)/(auth)/auth-success/page.tsx
+++ src/app/(root)/(auth)/auth-success/page.tsx
@@ -7,12 +7,13 @@ import { useRouter, useSearchParams } from "next/navigation";
 const AuthSuccess = () => {
   const searchParams = useSearchParams();
   const token = searchParams.get("token") || null;
+  const refreshToken = searchParams.get("refreshtoken") || null
   const router = useRouter()
 
   const setLoggedIn = useBoundStore((state) => state.setLoggedIn);
 
-  if (token) {
-    setLoggedIn({token: token});
+  if (token && refreshToken) {
+    setLoggedIn({token: token, refreshToken: refreshToken});
     router.replace(ROUTES.HOME)
   }else{
     router.back()
